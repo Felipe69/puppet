f13139b18c97b80ad6c075d88d065f0cf3a82c33
Felipe Aguiar <felipeaguiar@lnx01.interop.com.br>
2018-09-27 11:36:56 -0300

    add service

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,15 +1,25 @@
-package {'sshdd'
-if $::osfamily == 'RedHat  {
+case $::operatingsystem {
+	fedora: { $openssh = "openssh-server"}
+	centos: { $openssh = "openssh-server" }
+	ubuntu: { $openssh = "ssh"}
+}
+
+package {'openssh':
+	name => $openssh,
+	ensure => latest,
+}
+
+if $::osfamily == 'RedHat' {
 	service {'sshd':
 	ensure => 'running',
 	enable => true,
+	require => Package['openssh'],
 	}
 }
-else $::osfamily == 'Debian' {
-	service {'ssh':
+elsif $::osfamily == 'Debian' {
+	service {'openssh':
 	ensure => 'running',
 	enable => true,
+	require => Package['ssh'],
 	}
 }
-
-
